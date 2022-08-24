060fbed7a67c290f9122e7cd4b7182b7b062e540
Zeirat <lucasmeninno@outlook.com>
2022-08-24 12:55:58 -0300

    1.0.17

--- inmobiliaria/assets/js/slider.js
+++ inmobiliaria/assets/js/slider.js
@@ -2,8 +2,9 @@ window.addEventListener('load', function () {
     new Glider(document.querySelector('.slider'), {
         slidesToShow: 3,
         slidesToScroll: 2,
-        draggable: false,
+        draggable: true,
         rewind: true,
+        dragVelocity: 2,
         arrows: {
             prev: '.slider-btn-left',
             next: '.slider-btn-right'
